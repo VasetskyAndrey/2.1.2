9269c28b5c14ac22b74976cb9b7fab3da9ca9911
Vasetsky Andrey <regboxforu@gmail.com>
2024-03-22 20:07:24 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld HelloBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld HelloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(HelloBean1.equals(HelloBean2));
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.equals(catBean2));
+
+
+
+    }
+}
